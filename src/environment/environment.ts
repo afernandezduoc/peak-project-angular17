dab0a3d6fe2aba0fa7ae50f5c1703064bc30caac
Alejandro Fernández <alej.fernandez@duocuc.cl>
2024-07-09 00:11:01 -0400

    Se genera proyecto con Angular 17 y se instala Firebase

---
+++ src/environment/environment.ts
@@ -0,0 +1,13 @@
+export const environment = {
+    production: false,
+    firebaseConfig: {
+      apiKey: "AIzaSyAkaRAyB3K05iNCIxuaeT8XzwX2sArzX8Y",
+      authDomain: "peak-project-f7f92.firebaseapp.com",
+      databaseURL: "https://peak-project-f7f92-default-rtdb.firebaseio.com",
+      projectId: "peak-project-f7f92",
+      storageBucket: "peak-project-f7f92.appspot.com",
+      messagingSenderId: "478222275729",
+      appId: "1:478222275729:web:c05e80b84bcae53fac5b30",
+      measurementId: "G-JPV3DBWH5X"
+    }
+  };
